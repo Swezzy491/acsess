286f185b1a3903003e18ddcbaf754349d6955ddc
Swezzy491 <semihbilgin0534@gmail.com>
2024-10-17 15:04:04 +0300

    Add files via upload

---
+++ vt_baglanti/vt_baglanti/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// bURAYI UNUTMA
+using System.Data.OleDb;
+
+namespace vt_baglanti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Bağlantı oluştur
+        OleDbConnection bgl = new OleDbConnection("Provider=Microsoft.Jet.Oledb.4.0;Data Source=vt.mdb");
+        void formgoster()
+        {
+        bgl.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("SELECT * FROM tablo1", bgl);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            bgl.Close();
+            }
+    
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formgoster();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+            OleDbCommand sqlsorgu = new OleDbCommand(" INSERT INTO Tablo1 (ogr_no,adSoyad) VALUES('"+textBox1.Text+"','"+textBox2.Text+"')",bgl);
+            bgl.Open();
+            sqlsorgu.ExecuteNonQuery();
+            bgl.Close();
+
+          
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            OleDbCommand sqlsorgu = new OleDbCommand(" DELETE FROM Tablo1  WHERE Kimlik = " + textBox3.Text,bgl);
+            bgl.Open();
+            sqlsorgu.ExecuteNonQuery();
+            bgl.Close();
+            formgoster();
+            textBox3.Text = "";
+
+        }
+    }
+}
---
+++ vt_baglanti/vt_baglanti/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("vt_baglanti")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("vt_baglanti")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("3d8e7990-0f23-4052-bbeb-2f40a4ef2499")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
